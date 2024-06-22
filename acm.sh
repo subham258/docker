30272e8700d8ca8e3bb13ff00881df1ef0a21156
subham258 <95086425+subham258@users.noreply.github.com>
2024-06-22 16:48:57 +0530

    Update acm.sh

--- acm.sh
+++ acm.sh
@@ -3,14 +3,30 @@
 #!/bin/bash
 
 # Set AWS region
-region="ap-south-1"  # Replace with your desired region
+#region="ap-south-1"  # Replace with your desired region
 
 # Set variables for filtering ACM certificates
-app="infra"  # Replace with the tag value you want to filter by
-environment="DEV"  # Replace with the tag value you want to filter by
+#app="infra"  # Replace with the tag value you want to filter by
+#environment="DEV"  # Replace with the tag value you want to filter by
+
+# AWS CLI command to list ACM certificates based on tags
+#certificate_list=$(aws acm list-certificates --region $region --query "CertificateSummaryList[?Tags[?Key=='Owner'&&Value=='$app'&&Key=='Environment'&&Value=='$environment']].CertificateArn" --output text)
+
+#echo "ACM Certificates for App: $app, Environment: $environment"
+#echo "$certificate_list"
+
+
+
+#!/bin/bash
+
+# Usage: acm_certificate_details.sh <region> <app> <environment>
+
+region="$1"
+app="$2"
+environment="$3"
 
 # AWS CLI command to list ACM certificates based on tags
 certificate_list=$(aws acm list-certificates --region $region --query "CertificateSummaryList[?Tags[?Key=='Owner'&&Value=='$app'&&Key=='Environment'&&Value=='$environment']].CertificateArn" --output text)
 
-echo "ACM Certificates for App: $app, Environment: $environment"
 echo "$certificate_list"
+
