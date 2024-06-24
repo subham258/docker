06f4c19b00d264b3f618d32649bbfff16d70e769
subham258 <95086425+subham258@users.noreply.github.com>
2024-06-25 00:59:32 +0530

    Update acm_certificate_details2.sh

--- acm_certificate_details2.sh
+++ acm_certificate_details2.sh
@@ -33,9 +33,14 @@ environment="$3"
 
 # AWS CLI command to list ACM certificates based on tags
 #certificate_arns=$(aws acm list-certificates --query 'CertificateSummaryList[].CertificateArn' --region $region --output text)
-aws acm list-certificates --region $region --output text
+aws acm list-certificates --output text \
+  | awk '{print $1, $4, $5}' \
+  | column -t
+
+echo "Listed all your AWS ACM certificates."
+
+exit 0
 
 
-echo "$certificate_arns"
 
 
